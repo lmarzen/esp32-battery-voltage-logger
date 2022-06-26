f2bd2dd1a867e047ff706bc67eb7ebd5b84c55bc
Luke Marzen <marzenlj@gmail.com>
2022-06-26 01:00:42 -0700

    formatting

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -3,22 +3,23 @@
 #include <nvs_flash.h>
 #include <WiFi.h>
 
-
 #define ADC_PIN 15
 #define MAX_READINGS 256
 
 Preferences prefs;
 
 // clear all non-volatile storage
-void reformat_nvs() {
+void reformat_nvs()
+{
   nvs_flash_erase(); // erase the NVS partition and...
-  nvs_flash_init(); // initialize the NVS partition.
-  while(true);
+  nvs_flash_init();  // initialize the NVS partition.
+  while (true);
 }
 
-void setup() {
-  //esp_sleep_enable_timer_wakeup((60*30) * 1000000LL);
-  //esp_deep_sleep_start();
+void setup()
+{
+  // esp_sleep_enable_timer_wakeup((60*30) * 1000000LL);
+  // esp_deep_sleep_start();
 
   Serial.begin(115200);
 
@@ -32,25 +33,25 @@ void setup() {
 
 bool isPrime(int32_t N)
 {
-    for(int32_t i = 2 ; i * i <= N ; i++)
-        if(N % i == 0)
-            return false;
-    return true;
+  for (int32_t i = 2; i * i <= N; i++)
+    if (N % i == 0)
+      return false;
+  return true;
 }
 int32_t countPrimes(int32_t N)
 {
-    if(N < 3)
-        return 0;
-    int32_t cnt = 1;
-    for(int32_t i = 3 ; i < N ; i += 2)
-        if(isPrime(i))
-            cnt++;
-    return cnt;
+  if (N < 3)
+    return 0;
+  int32_t cnt = 1;
+  for (int32_t i = 3; i < N; i += 2)
+    if (isPrime(i))
+      cnt++;
+  return cnt;
 }
 
 // Takes about 30 minutes for each loop
-void loop() 
-{ 
+void loop()
+{
   delay(1000);
 
   // Read existing values from non-volatile storage
@@ -63,14 +64,16 @@ void loop()
 
   // Print all readings to serial
   Serial.println("v---------------");
-  for (int i = 0; i <= index; ++i) {
+  for (int i = 0; i <= index; ++i)
+  {
     Serial.println(adc_readings[i]);
   }
   Serial.println("^---------------");
 
-  if (index >= MAX_READINGS) {
+  if (index >= MAX_READINGS)
+  {
     Serial.println("MAX READINGS REACHED!!! Going to sleep...");
-    esp_sleep_enable_timer_wakeup((60*30) * 1000000LL);
+    esp_sleep_enable_timer_wakeup((60 * 30) * 1000000LL);
     esp_deep_sleep_start();
   }
 
@@ -82,11 +85,12 @@ void loop()
 
   // Turn wifi on to increase power-draw
   WiFi.mode(WIFI_STA);
-  WiFi.begin("notarealssid","notarealpassword");
+  WiFi.begin("notarealssid", "notarealpassword");
 
   // Do some calculations to increase power-draw
   Serial.println("Calculating how many primes exist under 10,000,000... (14x)");
-  for (int j = 0; j < 14; ++j) {
+  for (int j = 0; j < 14; ++j)
+  {
     Serial.println(countPrimes(10000000L));
   }
 
